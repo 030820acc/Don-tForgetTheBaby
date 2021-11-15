a6a9804a97de80210ee1fe43a9d3f3b8a5f7750d
Kreston <kreston.cm@gmail.com>
2021-11-15 15:48:40 -0600

    Index.js and users.js complete

--- express-starter-8.6.0/express-project-starter/routes/index.js
+++ express-starter-8.6.0/express-project-starter/routes/index.js
@@ -1,9 +1,121 @@
 var express = require('express');
-var router = express.Router();
+const router = express.Router();
+const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+const db = require('../db/models');
+const sequelize = require('sequelize');
+const op = sequelize.Op
+const { loginUser, logoutUser } = require('../auth');
+
+const csrfProtection = csrf({ cookie: true });
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+const bcrypt = require('bcryptjs');
+
+
+
+// Signup
+router.get('/signup', csrfProtection, asyncHandler(async (req, res) => {
+  const user = await db.User.build();
+  res.render('signup', {
+    user,
+    csrfToken: req.csrfToken(),
+  });
+}));
+
+const userValidators = [
+  // Define the user validators.
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username.')
+    .isLength({ max: 50 })
+    .withMessage('Username must not be more than 50 characters long.')
+    .custom((value) => {
+      return db.User.findOne({ where: { username: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Username is already in use by another account. Please try again.');
+          }
+        })
+    }),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address.')
+    .isLength({ max: 320 })
+    .withMessage('Email Address must not be more than 320 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email. Please try again.')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account. Please try again.');
+          }
+        })
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password.')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long.'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password.')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long.')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password. Please try again');
+      }
+      return true;
+    }),
+];
+
+
+
+// After clicking signup button 
+router.post('/signup', csrfProtection, userValidators,
+  asyncHandler(async (req, res) => {
+    const { username, email, password, } = req.body;
+
+
+    const user = db.User.build({
+      username,
+      email,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.hashedPassword = hashedPassword;
+      await user.save();
+      // Create Personal list and Trash list for each new user
+      await db.UserList.create({ listName: 'Personal', userId: user.id });
+      await db.UserList.create({ listName: 'Trash', userId: user.id });
+      loginUser(req, res, user);
+      res.redirect(`/profile`);
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('signup', {
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+
+
+  }));
+
+
+// Logout 
+router.get('/logout', (req, res) => {
+    logoutUser(req, res);
+    res.render('logout');
 });
+
+
+
 
 module.exports = router;
--- express-starter-8.6.0/express-project-starter/routes/users.js
+++ express-starter-8.6.0/express-project-starter/routes/users.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const csrf = require('csurf');
 const bcrypt = require('bcryptjs')
-const {User} = ('../db/models')
+const db = ('../db/models')
+const sequelize = require('sequelize');
+
+const { check, validationResult } = require('express-validator');
+const op = sequelize.Op
+const { loginUser, logoutUser } = require('../auth');
+const csrfProtection = csrf({ cookie: true });
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+
 
 // const handleValidationErrors = (req, res, next) => {const validationErrors = validationResult(req)}
 
+router.get('/signup', csrfProtection, asyncHandler(async (req, res) => {
+  const user = await db.User.build();
+  res.render('signup', {
+    user,
+    csrfToken: req.csrfToken(),
+  })
+}))
 
 
-
-const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 /* GET users listing. */
 // const authorizer = async function (req, res, next) {
 //   const {hashedPassword} = req.body;
@@ -17,16 +32,16 @@ const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).ca
 //   next();
 // }
 //get login page and authorize the client 
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
   res.render('login');
 });
 
 router.post('/login', asyncHandler, async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ where: email})
-  const {hashedPassword} = user;
-  
+  const user = await User.findOne({ where: email })
+  const { hashedPassword } = user;
+
 
 });
 
