946f083e6e531b45e742d61dbfa81434eae4fa4f
030820acc <030820acc@gmail.com>
2021-11-15 16:09:54 -0500

    starting on routes

--- express-starter-8.6.0/express-project-starter/routes/users.js
+++ express-starter-8.6.0/express-project-starter/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs')
+const {User} = ('../db/models')
 
+// const handleValidationErrors = (req, res, next) => {const validationErrors = validationResult(req)}
+
+
+
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// const authorizer = async function (req, res, next) {
+//   const {hashedPassword} = req.body;
+//   const saltRounds = 10;
+//   const hash = await bcrypt.hash(password, saltRounds);
+//   next();
+// }
+//get login page and authorize the client 
+router.get('/login', function(req, res) {
+  res.render('login');
 });
 
+router.post('/login', asyncHandler, async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ where: email})
+  const {hashedPassword} = user;
+  
+
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500);
+  const isProduction = process.env.NODE_ENV === 'production';
+  res.render('error', {
+    title: 'Server Error',
+    message: isProduction ? null : err.message,
+    error: isProduction ? null : err,
+  });
+});
 module.exports = router;
