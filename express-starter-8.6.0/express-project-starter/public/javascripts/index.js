e480d282f1da0f243ffcaa45e7ca01aadd441bc5
Enoch Tan <enochtan@umich.edu>
2021-11-17 16:12:05 -0500

    deleted unnecessary comments/files and started event listeners

--- express-starter-8.6.0/express-project-starter/public/javascripts/index.js
+++ express-starter-8.6.0/express-project-starter/public/javascripts/index.js
@@ -1,3 +1,19 @@
+const db = require('../../db/models')
+
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
+})
+
+// const logout = document.querySelector('#logoutbutton')
+// redirect to login page?
+
+const addList = document.querySelector('#add-list-button')
+const newListValue = document.querySelector('.add-list-value')
+
+addList.addEventListener('click', async () => {
+    const { userId } = req.session.auth
+    const newList = await db.List.create({
+        listName: newListValue.value,
+        userID: userId
+    })
 })
