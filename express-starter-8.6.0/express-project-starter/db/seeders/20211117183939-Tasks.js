e429285c328a6e706a6fb69e1f7dc0488047bd50
Adam Cole <82151908+030820acc@users.noreply.github.com>
2021-11-17 19:20:12 -0500

    Update 20211117183939-Tasks.js

--- express-starter-8.6.0/express-project-starter/db/seeders/20211117183939-Tasks.js
+++ express-starter-8.6.0/express-project-starter/db/seeders/20211117183939-Tasks.js
@@ -1,23 +1,23 @@
 'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
-
       Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
       */
-   return queryInterface.bulkInsert('Tasks', [{
-     taskName: "Go to the store", listId: 1, createdAt: new Date(), updatedAt: new Date()
-   }], {});
+   return queryInterface.bulkInsert('Tasks', [
+     { taskName: 'Go to the store', listId: 4, userId: 1, createdAt: new Date(), updatedAt: new Date() }
+   ], {});
   },
-
   down: (queryInterface, Sequelize) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
-
       Example:
       */
    return queryInterface.bulkDelete('Tasks', null, {});
