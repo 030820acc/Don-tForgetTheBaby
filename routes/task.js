11fae59e38666142b62a6c9c212a93e9c5db489b
anthony <awadams198@gmail.com>
2021-11-19 17:36:48 -0500

    added delete route and button

---
+++ routes/task.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/models");
+const { requireAuth } = require("../auth");
+const { csrfProtection, asyncHandler } = require("./utils");
+
+// Create a new task
+
+router.get(
+  "/delete/:id(\\d+)",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const taskId = parseInt(req.params.id, 10);
+    const task = await db.Task.findByPk(taskId);
+    res.render("task-delete", {
+      task,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
+
+router.post(
+  "/delete/:id(\\d+)",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const taskId = parseInt(req.params.id, 10);
+    const tasks = await db.Task.findByPk(taskId);
+    await tasks.destroy();
+    res.redirect("/");
+  })
+);
+
+module.exports = router;
