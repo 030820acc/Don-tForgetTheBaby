cff308d5b3abc0a6d36f0149c4cadb6a2a21edce
Enoch Tan <enochtan@umich.edu>
2021-11-19 18:33:14 -0500

    pushed krestons changes

--- routes/list.js
+++ routes/list.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const { async } = require('regenerator-runtime');
@@ -6,6 +5,7 @@ const { requireAuth } = require('../auth')
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 const router = express.Router();
+
 const listValidators = [
   check('listName')
     .exists({ checkFalsy: true })
@@ -14,22 +14,23 @@ const listValidators = [
     .withMessage('List Name must not be more than 50 characters long')
 ];
 
-router.post('/lists/new', requireAuth, csrfProtection, listValidators,
-  asyncHandler(async (req, res) => {
+router.post('/lists/new', requireAuth, csrfProtection, listValidators, asyncHandler(async (req, res) => {
     const { userId } = req.session.auth;
     console.log(userId);
     const {
       listName
     } = req.body
+
     const validatorErrors = validationResult(req);
+
     if (validatorErrors.isEmpty()) {
       const list = await db.List.create({
         listName,
         userId: userId
       });
-      console.log("we're here")
+      // console.log("we're here")
       return res.redirect('/');
-    } else {
+    } else if (!validatorErrors.isEmpty()) {
       const errors = validatorErrors.array().map((error) => error.msg);
       res.redirect('/', {
         title: 'List',
@@ -42,14 +43,14 @@ router.post('/lists/new', requireAuth, csrfProtection, listValidators,
 router.get('/lists/:id', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
     console.log(req.params)
     const { id } = req.params
-    console.log(id)
+    // console.log(id)
     const { userId } = req.session.auth;
     const tasks = await db.Task.findAll({ where:
       {
         listId: id
       }
     })
-    console.log(tasks)
+    // console.log(tasks)
     const lists = await db.List.findAll({ where: { userId } })
     res.render('homepage', {
       title: 'Dashboard',
@@ -64,8 +65,8 @@ const taskValidators = [
   check('taskName')
     .exists({ checkFalsy: true })
     .withMessage('Please provide Task Description')
-    .isLength({ max: 50 })
-    .withMessage('Task description must not be more than 50 characters long')
+    .isLength({ max: 150 })
+    .withMessage('Task description must not be more than 150 characters long')
 ];
 
 router.post('/tasks/new', requireAuth, csrfProtection, taskValidators,
@@ -76,18 +77,22 @@ router.post('/tasks/new', requireAuth, csrfProtection, taskValidators,
       timeEstimate,
       list,
     } = req.body
-    const validatorErrors = validationResult(req);
+    // console.log(req.body)
+
+    const validatorErrors = validationResult(req)
+
     if (validatorErrors.isEmpty()) {
       const listObject = await db.List.findByPk(list);
-      console.log(listObject)
+      // console.log(listObject)
       const listId = listObject.id
 
       const task = await db.Task.create({
         taskName,
+        taskTime: timeEstimate,
         userId: userId,
         listId: listId
       });
-      console.log("we're here")
+      // console.log("we're here")
       return res.redirect('/');
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
@@ -101,4 +106,3 @@ router.post('/tasks/new', requireAuth, csrfProtection, taskValidators,
 
 
 module.exports = router;
-
