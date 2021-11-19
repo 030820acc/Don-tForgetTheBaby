29efa33533e20873a87068d806c88209e60067b2
Kreston <kreston.cm@gmail.com>
2021-11-19 15:58:28 -0600

    Ability to add tasks via list.js

--- routes/list.js
+++ routes/list.js
@@ -12,6 +12,10 @@ const listValidators = [
     .isLength({ max: 50 })
     .withMessage('List Name must not be more than 50 characters long')
 ];
+
+
+
+
 router.post('/lists/new', requireAuth, csrfProtection, listValidators,
   asyncHandler(async (req, res) => {
     const { userId } = req.session.auth;
@@ -37,4 +41,44 @@ router.post('/lists/new', requireAuth, csrfProtection, listValidators,
     }
   }));
 
-  module.exports = router;
+const taskValidators = [
+  check('taskName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide Task Description')
+    .isLength({ max: 50 })
+    .withMessage('Task description must not be more than 50 characters long')
+];
+
+router.post('/tasks/new', requireAuth, csrfProtection, taskValidators,
+  asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
+    const {
+      taskName,
+      timeEstimate,
+      list,
+    } = req.body
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty()) {
+      const listObject = await db.List.findByPk(list);
+      console.log(listObject)
+      const listId = listObject.id
+
+      const task = await db.Task.create({
+        taskName,
+        userId: userId,
+        listId: listId
+      });
+      console.log("we're here")
+      return res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.redirect('/', {
+        title: 'List',
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
+
+
+module.exports = router;
